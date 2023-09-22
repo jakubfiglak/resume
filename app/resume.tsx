d9ceef5b3a2816538964d672eff6dfcacbc560c7
jakubfiglak <jakub.figlak@netguru.com>
2023-09-22 20:16:08 +0200

    feat: layout

--- app/resume.tsx
+++ app/resume.tsx
@@ -2,8 +2,8 @@
 
 import { technologies } from "@/app/data";
 import {
+  CalendarDaysIcon,
   GithubIcon,
-  Linkedin,
   LinkedinIcon,
   MailIcon,
   PhoneIcon,
@@ -12,6 +12,7 @@ import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
 
 export const Resume = () => {
   const resumeRef = useRef(null);
@@ -102,9 +103,124 @@ export const Resume = () => {
               </li>
             </ul>
           </section>
+          <section>
+            <h2 className="text-lg font-bold mb-2">Education</h2>
+            <ul className="text-xs space-y-1">
+              <li>
+                <p>Bachelor of Engineering, Electrotechnics</p>
+                <p className="text-muted-foreground">
+                  October 2013 - June 2018, Poznan University of Technology -
+                  Poznan, Poland
+                </p>
+              </li>
+              <li>
+                <p>Master of Engineering, Civil Engineering</p>
+                <p className="text-muted-foreground">
+                  October 2008 - June 2013, Poznan University of Technology -
+                  Poznan, Poland
+                </p>
+              </li>
+            </ul>
+          </section>
         </aside>
 
-        <section className="flex-1 p-4">whatever</section>
+        <section className="flex-1 p-4">
+          <div className="text-xs text-muted-foreground">
+            <h2 className="text-xl font-bold uppercase text-foreground">
+              Professional experience
+            </h2>
+            <Separator className="h-[2px] bg-primary mb-3" />
+            <div>
+              <h3 className="font-bold text-base text-foreground">
+                Frontend Developer
+              </h3>
+              <a href="https://www.netguru.com/" className="font-bold text-sm">
+                Netguru
+              </a>
+              <div className="flex items-center gap-1 text-xs text-muted-foreground my-1">
+                <CalendarDaysIcon className="h-4 w-4" /> October 2021 - Ongoing
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aspernatur pariatur labore, perferendis numquam, quibusdam
+                dolorum dolores eligendi placeat voluptatem culpa consequatur
+                vero ab aperiam tempora officiis sequi nesciunt alias. Nam,
+                similique nemo. Aliquam obcaecati expedita cumque nulla illo
+                blanditiis aut, eos perspiciatis unde impedit esse qui. Animi
+                ullam quae ab.
+              </p>
+              <ul className="list-disc list-inside">
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+            </div>
+            <Separator className="my-2" />
+            <div>
+              <h3 className="font-bold text-base text-foreground">
+                Production Manager
+              </h3>
+              <a
+                href="https://www.wega-elektronik.pl/pl/home"
+                className="font-bold text-sm"
+              >
+                WEGA Elektronik
+              </a>
+              <div className="flex items-center gap-1 text-xs text-muted-foreground my-1">
+                <CalendarDaysIcon className="h-4 w-4" /> September 2013 -
+                September 2021
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aspernatur pariatur labore, perferendis numquam, quibusdam
+                dolorum dolores eligendi placeat voluptatem culpa consequatur
+                vero ab aperiam tempora officiis sequi nesciunt alias. Nam,
+                similique nemo. Aliquam obcaecati expedita cumque nulla illo
+                blanditiis aut, eos perspiciatis unde impedit esse qui. Animi
+                ullam quae ab.
+              </p>
+              <ul className="list-disc list-inside">
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+            </div>
+
+            <h2 className="text-xl font-bold uppercase text-foreground mt-6">
+              Major side projects
+            </h2>
+            <Separator className="h-[2px] bg-primary mb-3" />
+            <div>
+              <a
+                href="https://www.wega-elektronik.pl/pl/home"
+                className="font-bold text-sm"
+              >
+                Scoutmaker Pro
+              </a>
+              <div className="flex items-center gap-1 text-xs text-muted-foreground my-1">
+                <CalendarDaysIcon className="h-4 w-4" /> May 2020 - December
+                2022
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aspernatur pariatur labore, perferendis numquam, quibusdam
+                dolorum dolores eligendi placeat voluptatem culpa consequatur
+                vero ab aperiam tempora officiis sequi nesciunt alias. Nam,
+                similique nemo. Aliquam obcaecati expedita cumque nulla illo
+                blanditiis aut, eos perspiciatis unde impedit esse qui. Animi
+                ullam quae ab.
+              </p>
+              <ul className="list-disc list-inside">
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+                <li>Lorem ipsum dolor sit amet.</li>
+              </ul>
+            </div>
+          </div>
+        </section>
       </article>
       <button
         onClick={() => {
