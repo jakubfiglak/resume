862dfc398a69d88bb6da3d24d89c987c691675b9
jakubfiglak <jakub.figlak@netguru.com>
2023-09-22 19:40:22 +0200

    feat: aside

---
+++ app/data.ts
@@ -0,0 +1,83 @@
+import next from "./icons/next.svg";
+import html from "./icons/html.svg";
+import css from "./icons/css.svg";
+import js from "./icons/javascript.svg";
+import ts from "./icons/typescript.svg";
+import contentful from "./icons/contentful.svg";
+import express from "./icons/express.svg";
+import gatsby from "./icons/gatsby.svg";
+import graphql from "./icons/graphql.svg";
+import nest from "./icons/nestjs.svg";
+import node from "./icons/node.svg";
+import payload from "./icons/payloadcms.svg";
+import postgres from "./icons/postgresql.svg";
+import react from "./icons/react.svg";
+import sanity from "./icons/sanity.svg";
+import redwood from "./icons/redwoodjs.svg";
+
+export const technologies = [
+  {
+    name: "HTML",
+    icon: html,
+  },
+  {
+    name: "CSS",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: js,
+  },
+  {
+    name: "TypeScript",
+    icon: ts,
+  },
+  {
+    name: "React",
+    icon: react,
+  },
+  {
+    name: "Next.js",
+    icon: next,
+  },
+  {
+    name: "Gatsby",
+    icon: gatsby,
+  },
+  {
+    name: "RedwoodJS",
+    icon: redwood,
+  },
+  {
+    name: "Sanity",
+    icon: sanity,
+  },
+  {
+    name: "Contentful",
+    icon: contentful,
+  },
+  {
+    name: "Payload CMS",
+    icon: payload,
+  },
+  {
+    name: "NodeJS",
+    icon: node,
+  },
+  {
+    name: "Express",
+    icon: express,
+  },
+  {
+    name: "GraphQL",
+    icon: graphql,
+  },
+  {
+    name: "NestJS",
+    icon: nest,
+  },
+  {
+    name: "PostgreSQL",
+    icon: postgres,
+  },
+];
--- app/resume.tsx
+++ app/resume.tsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { technologies } from "@/app/data";
+import {
+  GithubIcon,
+  Linkedin,
+  LinkedinIcon,
+  MailIcon,
+  PhoneIcon,
+} from "lucide-react";
 import { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
 
 export const Resume = () => {
   const resumeRef = useRef(null);
@@ -11,11 +21,99 @@ export const Resume = () => {
     <div>
       <article
         ref={resumeRef}
-        className="border border-slate-500 w-[21cm] h-[29.7cm] flex justify-center items-center"
+        className="border border-slate-500 w-[21cm] h-[29.7cm] flex"
       >
-        <h1>Hello</h1>
+        <aside className="bg-primary basis-2/5 p-4 space-y-6">
+          <header>
+            <h1 className="text-4xl font-extrabold tracking-tight">
+              Jakub Figlak
+            </h1>
+            <p className="text-muted-foreground">Frontend Developer</p>
+          </header>
+          <section>
+            <h2 className="text-lg font-bold mb-2">Contact</h2>
+            <ul className="text-xs space-y-2">
+              <li>
+                <a href="tel:+48792475974" className="flex items-center gap-2">
+                  <PhoneIcon className="h-4 w-4" /> +48 792 475 974
+                </a>
+              </li>
+              <li>
+                <a
+                  href="mailto:jakub.figlak@gmail.com"
+                  className="flex items-center gap-2"
+                >
+                  <MailIcon className="h-4 w-4" /> jakub.figlak@gmail.com
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/jakubfiglak"
+                  className="flex items-center gap-2"
+                >
+                  <GithubIcon className="h-4 w-4" /> GitHub
+                </a>
+              </li>
+              <li className="flex items-center gap-2">
+                <a
+                  href="https://www.linkedin.com/in/jakubfiglak/"
+                  className="flex items-center gap-2"
+                >
+                  <LinkedinIcon className="h-4 w-4" /> LinkedIn
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h2 className="text-lg font-bold mb-2">Summary</h2>
+            <p className="text-sm text-justify">
+              Skilled Frontend developer with 3+ years of professionale
+              experience building cutting-edge web applications for startups and
+              corporate clients. Proficient with CSS and JavaScript frameworks,
+              dedicated to delivering exceptional user experiences. Strongly
+              enthusiastic for learning new skills, staying up-to-date with the
+              latest industry trends and technologies. Combines technical
+              expertise with a business-oriented perspective.
+            </p>
+          </section>
+          <section>
+            <h2 className="text-lg font-bold mb-2">Tech Stack</h2>
+            <ul className="flex flex-wrap gap-2">
+              {technologies.map(({ name, icon }) => (
+                <li key={name}>
+                  <Badge variant="secondary" className="gap-1">
+                    <Image src={icon} alt={name} className="h-4 w-4" />
+                    {name}
+                  </Badge>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section>
+            <h2 className="text-lg font-bold mb-2">Languages</h2>
+            <ul className="text-sm">
+              <li className="flex justify-between">
+                <span>🇵🇱 Polish</span>
+                <span className="text-muted-foreground">Native</span>
+              </li>
+              <li className="flex justify-between">
+                <span>🇬🇧 English</span>
+                <span className="text-muted-foreground">C1</span>
+              </li>
+            </ul>
+          </section>
+        </aside>
+
+        <section className="flex-1 p-4">whatever</section>
       </article>
-      <button onClick={handlePrint}>Print</button>
+      <button
+        onClick={() => {
+          console.log("whatever?!");
+          handlePrint();
+        }}
+      >
+        Print
+      </button>
     </div>
   );
 };
